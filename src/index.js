c6f1e4c47d13b2e546115bdfdeb3dbdb00d02458
AshDoesStuf <ashplayzboi@gmail.com>
2023-04-02 07:31:59 +0200

    push

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
+const { RGBot } = require('rg-bot');
+const { RGCTFUtils, CTFEvent } = require('rg-ctf-utils');
+
+
+
+let rgCtfUtils = null;
 /**
  * @param {RGBot} rgbot
  */
@@ -6,10 +12,15 @@ function configureBot(rgbot) {
   // logs are displayed within the Regression Games app during a match
   rgbot.setDebug(true);
 
+  rgCtfUtils = new RGCTFUtils(bot)
+  rgCtfUtils.setDebug(true)
+
   // announce in chat when Bot spawns
   rgbot.on("spawn", function () {
     rgbot.chat("Hello World");
   });
+
+  
 }
 
 exports.configureBot = configureBot;
